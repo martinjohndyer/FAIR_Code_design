a567dee26128ed753c9762924e2cca5f293dc207
Romain Thomas <romain.thomas@sheffield.ac.uk>
2024-06-20 12:22:35 +0100

    correct config

--- Quiz_codes/Not_maintainable.py
+++ Quiz_codes/Not_maintainable.py
@@ -1,3 +1,7 @@
+
+
+
+
 def process_list(data):
     return [x * 2 + 3 for x in data if x % 2 == 0]
 
@@ -5,3 +9,10 @@ def process_list(data):
 input_data = [1, 2, 3, 4, 5, 6]
 result = process_list(input_data)
 print("Processed list:", result)
+
+
+
+
+
+
+
