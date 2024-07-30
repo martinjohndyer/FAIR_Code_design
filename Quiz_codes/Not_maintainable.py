cf00d17f5b52b568fe02c6c2c7a2d1aa65442e70
Romain Thomas <romain.thomas@sheffield.ac.uk>
2024-07-30 15:47:33 +0100

    Reshuffle chapters

--- Quiz_codes/Not_maintainable.py
+++ Quiz_codes/Not_maintainable.py
@@ -1,9 +1,7 @@
-
-
-
+import numpy
 
 def process_list(data):
-    return [x * 2 + 3 for x in data if x % 2 == 0]
+    return [2 *numpy.sqrt(x) + 3 for x in data if x * 1.5 < 5]
 
 # Example usage
 input_data = [1, 2, 3, 4, 5, 6]
